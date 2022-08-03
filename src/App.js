561feb358bfa09eaa4d6cd35437051bfd6d14ce1
rudans987 <rudans987@naver.com>
2022-08-03 14:36:15 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {TextArea, Input, Button} from "./components"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <TextArea />
+      </div>
+      <div>
+        <Input/>
+        <Button/>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
